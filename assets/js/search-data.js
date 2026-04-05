5fe82d7f75cdac1a460a1131337ef8cac575f6aa
lzeng9 <10093162+lzeng9@users.noreply.github.com>
2026-04-05 14:17:20 +0000

    Deploying to gh-pages from @ lzeng9/lzeng9.github.io@24b75bb491e26775c6807edaab8bee838f43e35b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -482,14 +482,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/zeng_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%4C.%5A%65%6E%67@%74%69%6C%62%75%72%67%75%6E%69%76%65%72%73%69%74%79.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
