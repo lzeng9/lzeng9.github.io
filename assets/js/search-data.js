d389a08b8d929c32e5adc7302f1a274a897570c0
lzeng9 <10093162+lzeng9@users.noreply.github.com>
2026-04-05 18:24:50 +0000

    Deploying to gh-pages from @ lzeng9/lzeng9.github.io@2262638502a7e6d71b8b9d412c8ad1c91cd4319c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
